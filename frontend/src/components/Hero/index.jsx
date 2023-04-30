7b74bedfd61a9c90bc50f45c7e7fab08c845851b
Jorge Calderón <Jorgedcalderon1989@gmail.com>
2023-04-29 21:44:21 -0300

    dev

---
+++ frontend/src/components/Hero/index.jsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <>
+        <div className="h-[100vh] overflow-hidden">
+            <img src="/img/demo-1.jpg" alt="demo photo" />
+        </div>
+    </>
+  )
+}
+
+export default Hero;
